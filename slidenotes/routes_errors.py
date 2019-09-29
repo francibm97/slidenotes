71755110ca5e981d9006798167cb3c6f830ea69e
Francesco <francesco.biancodev@gmail.com>
2019-09-29 16:56:12 +0200

    Fix errori

--- slidenotes/routes_errors.py
+++ slidenotes/routes_errors.py
@@ -1,15 +1,19 @@
 from werkzeug.exceptions import HTTPException
-from flask import render_template
+from flask import render_template, redirect
 
 from slidenotes import app
 from slidenotes.utils.clientinfo import client_wants_json
 from slidenotes.utils.responses import jsonify_error
 
-@app.errorhandler(HTTPException)
+@app.errorhandler(Exception)
 def error(error):
     if not isinstance(error, HTTPException):
+        app.logger.exception(error)
         error.code = 500
         error.description = "A causa di un errore interno dell'applicazione, la richiesta non può essere portata a termine"
+
+    if error.code == 301:
+        return redirect(error.new_url, code=301)
 
     if error.code == 404:
         error.description = "Ops, la pagina richiesta non è stata trovata"
